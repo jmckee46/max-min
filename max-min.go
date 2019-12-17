879738c17b26d3ea25d6b5a8e747e54d4adc25cf
John McKee <jmckee3@mac.com>
2019-12-16 19:58:40 -0800

    working version with logging

--- max-min.go
+++ max-min.go
@@ -1,3 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+type Arr []int32
+
+func (a Arr) Len() int           { return len(a) }
+func (a Arr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Arr) Less(i, j int) bool { return a[i] < a[j] }
+
+func maxMin(k int32, arr []int32) int32 {
+	arrLength := int32(len(arr))
+	myArr := Arr(arr)
+	var unfairness int32
+	var temp int32
+
+	sort.Sort(myArr)
+	fmt.Println("")
+	fmt.Println("k:", k)
+	fmt.Println("myArr:", myArr)
+
+	unfairness = myArr[k-1] - myArr[0]
+
+	for i := int32(0); i <= arrLength-k; i++ {
+		fmt.Println(myArr[i], myArr[i+k-1], "=", (myArr[i+k-1] - myArr[i]))
+		temp = myArr[i+k-1] - myArr[i]
+		if temp < unfairness {
+			unfairness = temp
+		}
+	}
+
+	return unfairness
+}
+
 func main() {}
--- spec-max-min_test.go
+++ spec-max-min_test.go
@@ -1,7 +1,53 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestMaxMin(t *testing.T) {
+func TestMaxMin0(t *testing.T) {
+	k := int32(3)
+	arr := []int32{10, 100, 300, 200, 1000, 20, 30}
+
+	unfairness := maxMin(k, arr)
+
+	if unfairness != 20 {
+		t.Errorf("got %d instead of 20", unfairness)
+	}
+
+}
+
+func TestMaxMin1(t *testing.T) {
+	k := int32(4)
+	arr := []int32{1, 2, 3, 4, 10, 20, 30, 40, 100, 200}
+
+	unfairness := maxMin(k, arr)
+
+	if unfairness != 3 {
+		t.Errorf("got %d instead of 3", unfairness)
+	}
+
+}
+
+func TestMaxMin2(t *testing.T) {
+	k := int32(2)
+	arr := []int32{1, 2, 1, 2, 1}
+
+	unfairness := maxMin(k, arr)
+
+	if unfairness != 0 {
+		t.Errorf("got %d instead of 0", unfairness)
+	}
+
+}
+
+func TestMaxMin3(t *testing.T) {
+	k := int32(5)
+	arr := []int32{4504, 1520, 5857, 4094, 4157, 3902, 822, 6643, 2422, 7288, 8245, 9948, 2822, 1784, 7802, 3142, 9739, 5629, 5413, 7232}
+
+	unfairness := maxMin(k, arr)
+
+	if unfairness != 1335 {
+		t.Errorf("got %d instead of 1335", unfairness)
+	}
 
 }
